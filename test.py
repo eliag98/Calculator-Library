de5d6882465ae2f904b5a6ab3a1491bd51050140
eliag98 <90949978+eliag98@users.noreply.github.com>
2024-03-29 21:34:50 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,18 @@
+from CalcLib import calculator
+
+x1 = 10
+x2 = 50
+
+y1 = 45
+y2 = 5
+
+z1 = 60
+z2 = 60
+
+r1 = 20
+r2 = 5
+
+print(calculator.addition(x1, x2))
+print(calculator.subtraction(y1, y2))
+print(calculator.multiplication(z1, z2))
+print(calculator.division(r1, r2))
